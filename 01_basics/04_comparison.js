b14de0070f6bb92b2f034d1c27f1fb264d6c94c9
Soni-Deepak <deepaksoni00450420@gmail.com>
2025-11-03 18:10:49 +0000

    comparison of datatypes

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0); //false
+console.log(null == 0); //false
+console.log(null >= 0); //true (because it convert null into number 0)
+
+console.log(undefined == 0);
+console.log(undefined < 0);
+console.log(undefined > 0);
+
+
+//=== {strict check it dosenot change the datatype}
+
+console.log("2" === 2);
